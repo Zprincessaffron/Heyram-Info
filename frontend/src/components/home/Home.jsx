9308b94afa7209495502946af9507b24630218c0
PREMKUMAR5519 <premkumarananthan1@gmail.com>
2024-09-30 12:37:37 +0530

    home created

---
+++ frontend/src/components/home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import MainSection from './MainSection'
+import MainServices from './MainServices'
+
+function Home() {
+  return (
+    <div>
+        <MainSection/>
+        <MainServices/>
+    </div>
+  )
+}
+
+export default Home
---
+++ frontend/src/components/home/MainFlow.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function MainFlow() {
+  return (
+    <div>MainFlow</div>
+  )
+}
+
+export default MainFlow
