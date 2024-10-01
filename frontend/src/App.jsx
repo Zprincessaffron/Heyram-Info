73f11d104d5e0f4104e1d8f2b5889d55d85b979d
PREMKUMAR5519 <premkumarananthan1@gmail.com>
2024-10-01 10:33:37 +0530

    forms

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,8 @@ import WebDevelopment from './components/services/WebDevelopment'
 import AboutUs from './components/About/AboutUs'
 import { Route, Routes } from 'react-router-dom'
 import Contact from './components/contact/Contact'
+import Career from './components/career/Career'
+import JobDetail from './components/career/JobDetail'
 
 function App() {
   return (
@@ -14,6 +16,8 @@ function App() {
         <Route path='/web-development' element={<WebDevelopment/>}/>
         <Route path='/about-us' element={<AboutUs/>}/>
         <Route path='/contact-us' element={<Contact/>}/>
+        <Route path="/careers" element={<Career/>} />
+        <Route path="/apply/:jobId" element={<JobDetail />} />
 
       </Routes>
     </UserProvider>
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,11 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter as Router  } from 'react-router-dom'
+import axios from 'axios';
+
+// Setting up the Axios base URL globally
+axios.defaults.baseURL = 'http://localhost:3000';
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Router>
